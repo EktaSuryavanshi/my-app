8e4512aefe9652b534c9be09efb070c9d61cafa9
Ekta Suryavanshi <ekta.suryavanshi@ascendion.com>
2024-07-25 19:02:56 +0530

    Initialize project using Create React App

---
+++ First.js
@@ -0,0 +1,14 @@
+function MyButton(){
+    return (
+        <button>I am button</button>
+    )
+}
+
+export default function MyApp(){
+    return (
+        <div>
+            <h1>My App</h1>
+            <MyButton/>
+        </div>
+    )
+} 
